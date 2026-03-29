e114210104ebdf18d1316693b8ead509481539c2
Jared Rudolph <jkrudolph@outlook.com>
2026-03-29 14:15:33 -0400

    feat: initialize module with forward drawdown labels and run function

---
+++ src/macro_stress_forecaster/__init__.py
@@ -0,0 +1,4 @@
+from macro_stress_forecaster.forecaster import run
+from macro_stress_forecaster.labels import compute_forward_drawdown_labels
+
+__all__ = ["compute_forward_drawdown_labels", "run"]
